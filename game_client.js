29a5ce26e32bf2aa29254e6282750b86855358ac
Matt Carnovale <matthew.carnovale@gmail.com>
2017-08-20 16:17:49 -0700

    User answer is checked; Point value assigned; Answer Revealed on survey board.

--- game_client.js
+++ game_client.js
@@ -1,17 +1,57 @@
 var strAnswerArray = [];
+var intRoundNumber = 1;
+var value = 0;
+var score = 0;
+
 
 jQuery('document').ready(function(){
-  jQuery('.user-search')
-    .keydown(function(event){
-      if(event.which === 13){
+//Handle phrase submission
+jQuery('input')
+  .keydown(function(event){
+    if(event.which === 13){
+                                //Handle Phrase submission
+      if(jQuery(this).hasClass('user-search')){
         getAnswers();
         switchSearchToAnswer();
+      } else{                   //Handle User answers
+        var objAnswerResult = checkUserAnswer();
+        if(objAnswerResult.isCorrect){
+          clearAnswer();
+          revealAnswer(objAnswerResult.index);
+        }
       }
     }
-  );
+  })
 });
 
-var getAnswers = () => {
+//DOM manipulation
+var switchSearchToAnswer = function(){
+ jQuery('.user-search')
+  .toggleClass( 'user-search user-answer')
+  .attr('placeholder', 'how would google autocomplete your phrase?')
+  .val('');
+}
+
+var clearAnswer = function(){
+  jQuery('.user-answer').val('');
+}
+
+var revealAnswer = function(intIndex){
+  var strCardAnswered = '.cardCover'.concat(intIndex)
+  assignValue(intIndex);
+  jQuery(strCardAnswered)
+    .toggleClass('answered');
+  jQuery(strCardAnswered + ' > div')
+    .toggleClass('num-background-oval answer-with-score')
+    .text('')
+    .append('<span class=\'answer\'>' + strAnswerArray[i] + '</span')
+    .append('<span class=\'value\'>' + value +'</span');
+
+}
+
+
+//Ajax work
+var getAnswers = function(){
   var strUserRequest = jQuery('.user-search').val();
   console.log(strUserRequest);
   jQuery.ajax({
@@ -24,7 +64,53 @@ var getAnswers = () => {
   });
 }
 
-var formatAnswerArray = (strSuggestionArray, strUserRequest) => {
+//Helper Functions
+
+var assignValue = function (intIndex){
+  switch (intIndex) {
+    case 0:
+      value = 100 * intRoundNumber;
+      break;
+    case 1:
+      value = 80 * intRoundNumber;
+      break;
+    case 2:
+      value = 60 * intRoundNumber;
+      break;
+    case 3:
+      value = 50 * intRoundNumber;
+      break;
+    case 4:
+      value = 40 * intRoundNumber;
+      break;
+    case 5:
+      value = 30 * intRoundNumber;
+      break;
+    case 6:
+      value = 20 * intRoundNumber;
+      break;
+    default:
+      value = 10 * intRoundNumber;
+  }
+}
+
+var checkUserAnswer = function () {
+  var strUserAnswer = jQuery('.user-answer').val();
+  console.log(strUserAnswer);
+  for(i = 0; i < 8; ++i){
+    if(strUserAnswer.toLowerCase() === strAnswerArray[i]){
+      return {
+        'isCorrect': true,
+        'index': i
+      };
+    }
+  }
+  return{
+    'isCorrect': false
+  };
+}
+
+var formatAnswerArray = function(strSuggestionArray, strUserRequest){
   var regex = new RegExp(strUserRequest + ' ', 'i');
   var i,j;
   for(i = 0, j = 0; i < 8 ; ++i, ++j){
@@ -36,11 +122,4 @@ var formatAnswerArray = (strSuggestionArray, strUserRequest) => {
    strAnswerArray[i] = strAnswerArray[i].replace(regex,'');
   }
   console.log(strAnswerArray);
-}
-
-var switchSearchToAnswer = () => {
- jQuery('.user-search')
-  .toggleClass('user-answer')
-  .attr('placeholder', 'how would google autocomplete your phrase?')
-  .val('')
 }
