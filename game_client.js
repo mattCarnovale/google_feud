58be25d8a7bdabb76e62beb219f52bbdcd8b47c9
Matt Carnovale <matthew.carnovale@gmail.com>
2017-08-19 22:07:15 -0700

    User can enter a phrase and an Array of Answers is acquired from the server.

--- game_client.js
+++ game_client.js
@@ -1,12 +1,46 @@
-
+var strAnswerArray = [];
 
 jQuery('document').ready(function(){
-  jQuery('#search-bar')
+  jQuery('.user-search')
     .keydown(function(event){
       if(event.which === 13){
-        var strUserRequest = jQuery('#search-bar').val();
-        console.log(strUserRequest);
+        getAnswers();
+        switchSearchToAnswer();
       }
     }
   );
 });
+
+var getAnswers = () => {
+  var strUserRequest = jQuery('.user-search').val();
+  console.log(strUserRequest);
+  jQuery.ajax({
+    'type': 'GET',
+    'url': '/phrase?strUserRequest=' + encodeURIComponent(strUserRequest),
+    'dataType': 'json',
+    'success': function(strSuggestionArray){
+      formatAnswerArray(strSuggestionArray, strUserRequest);
+    }
+  });
+}
+
+var formatAnswerArray = (strSuggestionArray, strUserRequest) => {
+  var regex = new RegExp(strUserRequest + ' ', 'i');
+  var i,j;
+  for(i = 0, j = 0; i < 8 ; ++i, ++j){
+    //Sometimes the first suggestion is the phrase itself (boring)
+    if(strSuggestionArray[i] === strUserRequest){
+      ++j;
+    }
+   strAnswerArray[i] = strSuggestionArray[j];
+   strAnswerArray[i] = strAnswerArray[i].replace(regex,'');
+  }
+  console.log(strAnswerArray);
+}
+
+var switchSearchToAnswer = () => {
+ jQuery('.user-search')
+  .toggleClass('user-answer')
+  .attr('placeholder', 'how would google autocomplete your phrase?')
+  .val('')
+}
--- game_server.js
+++ game_server.js
@@ -6,13 +6,36 @@ var googleSuggest = require('suggestion');
 
 var server = express();
 
-server.use('/', express.static(__dirname + '/game_client.html'));
 
-var io = socket(server.listen(process.env.PORT || 8080));
+// ajax
+server.use('/', express.static(__dirname + '/'));
+server.listen(process.env.PORT || 8080);
 
-io.on('connection', function(objectSocket){
-    console.log('connected!')
+server.get('/phrase', function(request, response){
+  var strUserPhraseToSearch = request.query.strUserRequest;
+  console.log(strUserPhraseToSearch);
+  var strSuggestionArray;
+  googleSuggest(strUserPhraseToSearch, function(error, strSuggestionArray){
+    if(error) throw error;
+    console.log(strSuggestionArray);
+    response.status(200);
+    response.set({
+      'Content-Type' : 'application/json'
+    });
+    response.send(JSON.stringify(strSuggestionArray));
+  });
 
 });
 
-console.log('go ahead and open "http://localhost:8080/game_client.html"');
+
+//socket.io
+// server.use('/', express.static(__dirname + '/game_client.html'));
+
+// var io = socket(server.listen(process.env.PORT || 8080));
+//
+// io.on('connection', function(objectSocket){
+//     console.log('connected!')
+//
+// });
+//
+// console.log('go ahead and open "http://localhost:8080/game_client.html"');
